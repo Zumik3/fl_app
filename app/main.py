a4805fcbd087e57c045795ff381fabb92db108dc
zum <p.dinkevich@mail.ru>
2022-05-12 15:30:12 +0300

    decor

--- app/main.py
+++ app/main.py
@@ -1,34 +1,25 @@
-from flask import Flask, json, Response
-from support import resp
+from flask import Flask
 from initial import initialize
 from blueprints.external_access import external_access
 from blueprints.internal_access import internal_access
 from flask_bootstrap import Bootstrap5
-
 
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
 app.register_blueprint(external_access)
 app.register_blueprint(internal_access)
 
-JSON_DIR = './initial/'
-
 
 def initialize_all():
+    json_dir = './initial/'
 
-    initialize.initialize_table('User', f'{JSON_DIR}users.json')
-    initialize.initialize_table('Item', f'{JSON_DIR}items.json')
-    initialize.initialize_table('Image', f'{JSON_DIR}images.json')
-    initialize.initialize_table('Link', f'{JSON_DIR}links.json')
+    initialize.initialize_table('User', f'{json_dir}users.json')
+    initialize.initialize_table('Item', f'{json_dir}items.json')
+    initialize.initialize_table('Image', f'{json_dir}images.json')
+    initialize.initialize_table('Link', f'{json_dir}links.json')
 
 
 initialize_all()
-
-
-@app.route('/ping2/')
-def index2():
-
-    return resp(200, {'status': True})
 
 
 if __name__ == "__main__":
